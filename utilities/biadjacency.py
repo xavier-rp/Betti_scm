325a063f790e6d0f064ddd1a3f104d0c714e8c91
Xavier Roy-Pomerleau <xavier.roy-pomerleau.1@ulaval.ca>
2018-10-24 16:26:30 -0400

    Added documentation for this module.

--- utilities/biadjacency.py
+++ utilities/biadjacency.py
@@ -6,7 +6,19 @@ from sklearn.preprocessing import normalize
 import networkx.algorithms
 
 def to_nx_edge_list_format(filename_or_matrix='SubOtu4000.txt', out_name='edgelist_SubOtu4000.txt'):
-    #Takes a biadjacency matrix in a txt file OR a numpy matrix and writes it in the konect format (edgelist)
+    """
+    This function takes a biadjacency matrix in a txt file OR a numpy matrix/array and writes its edge list in the NetworkX
+    format to a file.
+    Parameters
+    ----------
+    filename_or_matrix (str OR numpy.array) : file name from which to open the matrix, or numpy matrix. The edge list
+                                              of this matrix is saved in out_name.
+    out_name (str) : path and name of the file in which we save the edge list
+
+    -------
+
+    """
+
 
     if isinstance(filename_or_matrix, str):
         ### Instruction : skiprows and usecols have to be updated depending on the file you use
@@ -24,20 +36,21 @@ def to_nx_edge_list_format(filename_or_matrix='SubOtu4000.txt', out_name='edgeli
     G = nx.algorithms.bipartite.from_biadjacency_matrix(A)
     nx.write_edgelist(G, out_name, data=False)
 
-
-def read_edge_list_konect_minus(path):
-    """Read edge list, but the nodes have to be zero indexed manually by specifying  -x, where x is an integer,
-    in the line edge_list.add((int(e[0]), int(e[1])-500)). In this example, the nodes of the second set had to be
-    reindexed using -500, because the smallest index for the second set was 500. """
-    with open(path, 'r') as f:
-        edge_list = set()
-        for line in f:
-            if not line.lstrip().startswith("%"):  # ignore commets
-                e = line.strip().split()
-                edge_list.add((int(e[0]), int(e[1]) - 47))  # 0 index
-        return list(edge_list)
-
 def normalize_columns(filename_or_matrix='SubOtu4000.txt', savename=None):
+    """
+    This function normalizes each column of the (biadjacency) matrix. It uses a function from sklearn.preprocessing
+    called normalize.
+
+    Parameters
+    ----------
+    filename_or_matrix (str OR numpy.array) : file name from which to open the matrix, or numpy matrix. The edge list
+                                              of this matrix is saved in out_name.
+    savename (str) : WIP Name under which to save the normalized matrix.
+
+    Return (Numpy array) : The matrix in which each column are normalized.
+    -------
+
+    """
     #Normalizes each column of a biadjacency matrix
     if savename is None:
         if isinstance(filename_or_matrix, str):
@@ -52,17 +65,50 @@ def normalize_columns(filename_or_matrix='SubOtu4000.txt', savename=None):
         normed_matrix = normalize(matrix, axis=0, norm='l1')
 
 def reduce_matrix(matrix):
-    # Removes lines that only contain zeros. Needed to make sure that everything gets properly zero-indexed.
+    """
+    This function removes the lines that only contain zeros from a matrix. This happens when a matrix was filtered using
+    one of the few functions of this module. As an example, if the matrix is a biadjacency matrix in which the lines
+    represent the species and the columns represent the site in which we find them, and we set to zero the proportion of
+    each specie that are lower than 0.05, we might create a line in which every element is zero.
+
+    If this line is not removed, it can cause indexing errors when writting the edgelist from such matrix.
+
+    Parameters
+    ----------
+    matrix numpy array : Matrix in which we want to remove the lines that only contain zeros.
+
+    Returns numpy array : Matrix in which we removed the lines.
+    -------
+
+    """
+
     i = 0
+    # Find the indices of the lines that only contain zeros
     index_list = []
     while i < matrix.shape[0]:
         if np.all(matrix[i,:] == 0):
             index_list.append(i)
         i += 1
+    # Delete these lines
     reduced_matrix = np.delete(matrix, index_list, axis=0)
     return reduced_matrix
 
 def matrix_filter(matrix, threshold=0.1, savename=None):
+    """
+    This function sets the elements of a matrix that are lower than ''threshold'' to zero and normalizes each column of
+    the matrix using normalize_columns().
+
+
+    Parameters
+    ----------
+    matrix numpy array : Matrix on which to apply filter
+    threshold (float) : Threshold under which the elements of the matrix are set to zero.
+    savename (str) : WIP TODO
+
+    Returns (numpy array) : The filtered matrix in which we renormalized each column.
+    -------
+
+    """
     # Sets the elements of the matrix to zero if it is lower than the threshold
     # and (re)normalizes each column of the matrix
 
@@ -76,19 +122,40 @@ def matrix_filter(matrix, threshold=0.1, savename=None):
         pass
 
 def matrix_filter_sum_to_prop(matrix, prop_threshold=0.5, allow_ties=True, savename=None):
-    # Sorts the elements of each column from highest to lowest proportion sums element by element until a propotion
-    # threshold is reached or bursted.
+    """
+    This function sorts the elements of each column by descending order and sums them until "prop_threshold" is reached
+    or bursted. It then keeps the '' n '' highest elements that were used to reach the threshold in each column and sets
+    every other element to zero. Finally, it normalizes each column of the matrix using normalize_columns().
 
-    # Keeps the n highest elements of each column of the matrix. If there are ties, it will also include them. It then
-    # (re)normalizes each column the matrix
+    In the context of biadjacency matrix in which each column is normalized, the number that we sum are proportion of a
+    specie in one site (columne) and sums to 1.
+
+    Parameters
+    ----------
+    matrix (numpy array) : Matrix on which to apply filter. Its columns must already be normalized
+                           (using normalize_columns()).
+    prop_threshold (float) : Number between 0 and 1 to which we sum the elements of a column. (0 for nothing, 1 for
+                             every elements)
+    allow_ties (bool) : If we reach the threshold and that the next element is equal to the last, we also keep it in
+                        the column, even though we already reached the threshold. This was considered since there might
+                        be two species that are present in the same proportion, but we don't want to pick at random the
+                        ones that are going to stay in the matrix.
+    savename (str) : WIP TODO
+
+    Returns (numpy array) : The filtered matrix in which we renormalized each column.
+    -------
+
+    """
+
 
     thresholdlist = []
     i = 0
-
+    # Sort each column in descending order
     while i < len(matrix[0, :]):
         sorted_column = sorted(matrix[:, i], reverse=True)
         total_sum = 0
         j = 0
+        # Sum the elements of the column until we burst the threshold
         while total_sum < prop_threshold:
             total_sum += sorted_column[j]
             j += 1
@@ -101,6 +168,9 @@ def matrix_filter_sum_to_prop(matrix, prop_threshold=0.5, allow_ties=True, saven
     nb_elements_in_column = len(matrix[:, i-1])
     if savename is None:
         i = 0
+        # For each column, we find the elements that are lower than the threshold found in the previous step and set
+        # them to zero. If allow_ties is False, we take the '' n '' elements that are equal and that would have made
+        # the program burst the threshold in the same way and pick one of them at random.
         while i < len(thresholdlist):
             print(thresholdlist[i])
             if len(np.where(matrix[:, i] == thresholdlist[i])) == 1:
@@ -129,8 +199,20 @@ def matrix_filter_sum_to_prop(matrix, prop_threshold=0.5, allow_ties=True, saven
 
 
 def matrix_filter_keep_n(matrix, n=1, savename=None):
-    # Keeps the n highest elements of each column of the matrix. If there are ties, it will also include them. It then
-    # (re)normalizes each column the matrix
+    """
+    This function keep the '' n '' highest elements of each column of the matrix. If there are ties, it will also
+    include them. All other elements in the column are then set to zero and each column is normalized.
+
+    Parameters
+    ----------
+    matrix (numpy array) : Matrix on which to apply filter.
+    n (int) : Number of highest elements to keep in each column
+    savename : WIP TODO
+
+    Returns (numpy array) : The filtered matrix in which we renormalized each column.
+    -------
+
+    """
 
     thresholdlist = []
     i = 0
@@ -157,16 +239,29 @@ def matrix_filter_keep_n(matrix, n=1, savename=None):
 
 
 def matrix_adaptive_filter(matrix, ntimes=0, savename=None):
-    # Sets the elements of the matrix to zero if it is lower than the threshold
-    # and (re)normalizes each column the matrix
-    # The adaptive filter plays around the mean. If ntime == 0, all species in a site that are present in a proportion
-    # lower than the mean are set to zero. If ntimes == negative integer, we do the same, but for species that are less
-    # present than mean - -ntimes*sigma (more restritive), where sigma is the standard variation. if ntimes == positive integer, we cut
-    # those under mean - ntimes*sigma (less restrictive)
+    """
+    This function sets the elements of a matrix that are lower than ''threshold'' to zero and normalizes each column of
+    the matrix using normalize_columns(). The threshold is computed from the mean of each column and the specified
+    number "ntimes" of standard deviation : threshold = mean of column - ntimes * standard deviation. Negative floats
+    are thus more restrictive (we keeps less and less species since the threshold is high) and positive floats are
+    more permissive.
+
+    Parameters
+    ----------
+    matrix (numpy array) : Matrix on which to apply filter
+    ntimes (float) : Number of times we multiply the standard deviation
+    savename WIP TODO
+
+    Returns (numpy array) : The filtered matrix in which we renormalized each column.
+    -------
+
+    """
+
     thresholdlist = []
     i = 0
     transpose = matrix.T
     print(np.count_nonzero(matrix))
+    # Compute the mean and standard deviation and put the resulting threshold in a list
     while i < len(transpose[:,0]):
 
         mean = np.mean(transpose[i,:])
@@ -237,38 +332,11 @@ if __name__ == '__main__':
     It can also normalize the matrix by column and remove values under a certain threshold in each column
     """
 
-
-
     matrix1 = np.loadtxt('final_OTU.txt', skiprows=0, usecols=range(1,39))
     mat = normalize_columns(matrix1)
-    i = 0
-    array = []
-    while i < mat.shape[1]:
-        array.append(max(mat[:,i]))
-        print(min(mat[:,i]), max(mat[:,i]), np.sum(mat[:,i]))
-        i += 1
-    mean = sum(array)/len(array)
-    print(mean)
-    print(np.count_nonzero(mat))
-    #matrix = matrix_adaptive_filter(mat, ntimes=-2)
-    #matrix = matrix_adaptive_filter(mat, ntimes=-10)
-    #matrix = matrix_filter_keep_n(mat, n=20)
-    #matrix = matrix_filter_sum_to_prop(mat, prop_threshold=0.2)
     matrix = matrix_filter(mat, threshold=0.04)
     print(matrix == mat)
     matrix = reduce_matrix(matrix)
-
-    print(to_nx_edge_list_format(matrix, out_name='edglis'))
-    #print('stop')
-
-    #polli = []
-    #with open("/home/xavier/Documents/Projet/scm/datasets/pollinators_facet_list.txt", 'r') as f:
-    #    for l in f:
-    #        polli.append([int(x) for x in l.strip().split()])
-    #G = facet_list_to_bipartite2(polli)
-    #nx.write_edgelist(G, 'polli_edge.txt', data=False)
+    to_nx_edge_list_format(matrix, out_name='edglis')
 
     print('Done.\n Don\'t forget to change the saving file name so your file does not get written over. ')
-
-
-    #to_konect_format(matrix, out_name='severeadaptivthresh_finalOTU.txt')
