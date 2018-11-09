663c65511b1bf2d37b14c735820a2b9d53f5bfd6
Xavier Roy-Pomerleau <xavier.roy-pomerleau.1@ulaval.ca>
2018-11-09 16:25:12 -0500

    Added a function to transform a facet list into its n-skeleton

--- Percolation.py
+++ Percolation.py
@@ -47,6 +47,7 @@ def destroy_highest_facet(facetlist, previ):
         else:
             i += 1
     previ = i
+    print(stop_decompostion)
     # If there are no more facets of size > 1, we need to stop decomposing, hence this condition
     if not stop_decompostion:
 
@@ -116,11 +117,11 @@ def destroy_random_facet(facetlist, previ):
 
     return updatedfacetlist, previ
 
-def random_facet_to_1_simplices(facetlist, previ=0):
+def random_facet_to_1_simplices_then_0(facetlist, previ=0):
     """
     Takes a random facet in a facet list and decomposes it in its N choose 2 faces, where N is the number of
-    nodes in the facet.  It then returns the updated facet list where the original facet is deleted and replaced
-    by its N choose 2 faces.
+    nodes in the facet. If the facet is of size 2, it decomposes it in 2 disconnected nodes. It then returns the updated
+    facet list where the original facet is deleted and replaced by its N choose 2 faces (or 2 nodes).
     Parameters
     ----------
     facetlist  (list of lists) : Facet list generated from the Null_model.py module
@@ -180,11 +181,11 @@ def random_facet_to_1_simplices(facetlist, previ=0):
 
     return updatedfacetlist, previ
 
-def highest_facet_to_1_simplices(facetlist, previ=0):
+def highest_facet_to_1_simplices_then_0(facetlist, previ=0):
     """
     Takes the biggest facet in a facet list and decomposes it in its N choose 2 faces, where N is the number of
-    nodes in the facet.  It then returns the updated facet list where the original facet is deleted and replaced
-    by its N choose 2 faces.
+    nodes in the facet. If the facet is of size 2, it decomposes it in its 2 nodes. It then returns the updated facet
+    list where the original facet is deleted and replaced by its N choose 2 faces (or 2 nodes).
     Parameters
     ----------
     facetlist  (list of lists) : Facet list generated from the Null_model.py module
@@ -239,6 +240,121 @@ def highest_facet_to_1_simplices(facetlist, previ=0):
             for facet in prune(facetlist):
                 print(facet)
                 updatedfacetlist.append(facet)
+
+    return updatedfacetlist, previ
+
+def random_facet_to_n_skeleton(facetlist, previ=0, n=1):
+    """
+    Takes a random facet in a facet list and decomposes it in its n-skeleton (N choose n+1 faces), where N is the number
+    of nodes in the facet and n is the specified skeleton dimension. The algorithm does not change facets of dimension
+    lower than n+1, meaning that these facets are going to stay the same. It then returns the updated facet list where
+    the random facet was removed and replaced by its N choose n+1 faces.
+    Parameters
+    ----------
+    facetlist  (list of lists) : Facet list generated from the Null_model.py module
+    previ (int) : This variable plays two roles. Its first role is to represent the first index at which we have a facet
+                  of length > 1 in the sorted (by length of facet) facetlist. For instance, in [[1], [2], [3, 4]], previ
+                  would be 2. Its second role is to indicate how many facet of size 1 we have, hence 2 in the previous
+                  example. previ is also returned so that if we use the updated facet list another time in this function,
+                  we can specify how many facet of size one we had in the previous iteration (hence the name prev i).
+                  this way, the algorithm does not have to start from 0 each time we want to know the number of facets of
+                  length 1. This information is mandatory, since the algorithm has to stop once the facet list only
+                  contains facets of size 1.
+
+    Returns
+    -------
+    facetlist (list of lists) : The updated facet list in which we deleted the randomly selected facet and added the
+                                N choose n+1 faces of the selected facet.
+    previ (int) : The number of facets of size n+1 before the update.
+    """
+    updatedfacetlist = []
+
+    facetlist.sort(key=len)
+
+    # this loop finds the number of facets of length > n+1 and stores it in the variable i.
+    go_on = True
+    stop_decompostion = False
+    i = previ
+    while go_on:
+        if i == len(facetlist):
+            # If there are no more facets of size > 1, we need to stop decomposing, hence this condition
+            stop_decompostion = True
+            break
+        if len(facetlist[i]) > n+1:
+            go_on = False
+        else:
+            i += 1
+    previ = i
+    # If there are no more facets of size > n+1, we need to stop decomposing, hence this condition
+    if not stop_decompostion:
+        # draw a facet > n+1 randomly
+        random_index = np.random.randint(i, len(facetlist))
+        facet = facetlist[random_index]
+
+        # delete the facet in the list and replace it by its N choose n+1 faces
+        del (facetlist[random_index])
+        for face in itertools.combinations(facet, n+1):
+            facetlist.append(frozenset(face))
+        for facet in prune(facetlist):
+            print(facet)
+            updatedfacetlist.append(facet)
+
+    return updatedfacetlist, previ
+
+def highest_facet_to_n_skeleton(facetlist, previ=0, n=1):
+    """
+    Takes the highest facet in a facet list and decomposes it in its n-skeleton (N choose n+1 faces), where N is the number
+    of nodes in the facet and n is the specified skeleton dimension. The algorithm does not change facets of dimension
+    lower than n+1, meaning that these facets are going to stay the same. It then returns the updated facet list where
+    the random facet was removed and replaced by its N choose n+1 faces.
+    Parameters
+    ----------
+    facetlist  (list of lists) : Facet list generated from the Null_model.py module
+    previ (int) : This variable plays two roles. Its first role is to represent the first index at which we have a facet
+                  of length > 1 in the sorted (by length of facet) facetlist. For instance, in [[1], [2], [3, 4]], previ
+                  would be 2. Its second role is to indicate how many facet of size 1 we have, hence 2 in the previous
+                  example. previ is also returned so that if we use the updated facet list another time in this function,
+                  we can specify how many facet of size one we had in the previous iteration (hence the name prev i).
+                  this way, the algorithm does not have to start from 0 each time we want to know the number of facets of
+                  length 1. This information is mandatory, since the algorithm has to stop once the facet list only
+                  contains facets of size 1.
+
+    Returns
+    -------
+    facetlist (list of lists) : The updated facet list in which we deleted the randomly selected facet and added the
+                                N choose n+1 faces of the selected facet.
+    previ (int) : The number of facets of size n+1 before the update.
+    """
+    updatedfacetlist = []
+
+    facetlist.sort(key=len)
+
+    # this loop finds the number of facets of length > n+1 and stores it in the variable i.
+    go_on = True
+    stop_decompostion = False
+    i = previ
+    while go_on:
+        if i == len(facetlist):
+            # If there are no more facets of size > 1, we need to stop decomposing, hence this condition
+            stop_decompostion = True
+            break
+        if len(facetlist[i]) > n+1:
+            go_on = False
+        else:
+            i += 1
+    previ = i
+    # If there are no more facets of size > n+1, we need to stop decomposing, hence this condition
+    if not stop_decompostion:
+
+        facet = facetlist[-1]
+
+        # delete the facet in the list and replace it by its N choose n+1 faces
+        del (facetlist[-1])
+        for face in itertools.combinations(facet, n+1):
+            facetlist.append(frozenset(face))
+        for facet in prune(facetlist):
+            print(facet)
+            updatedfacetlist.append(facet)
 
     return updatedfacetlist, previ
 
@@ -379,10 +495,41 @@ def higher_order_link_percolation(origin_path, save_path, perc_func=decompose_ra
             json.dump(savedfacetlist, outfile)
         i += 1
 
+def to_skeleton_percolation(origin_path, save_path, perc_func=random_facet_to_n_skeleton, n=1):
+    with open(origin_path, 'r') as file:
+        facetlist = json.load(file)
+
+    # This loop transforms the sub lists in the facet list into frozen sets.
+    indx = 0
+    while indx < len(facetlist):
+        facetlist[indx] = frozenset(facetlist[indx])
+        indx += 1
+
+    previ = 0
+    i = 1
+    print(max([len(facet) for facet in facetlist]))
+    while max(len(facet) for facet in facetlist) != n + 1:
+        facetlist, previ = perc_func(facetlist, previ, n)
+        with open(save_path + str(i) + '.json', 'w') as outfile:
+            indx = 0
+            savedfacetlist = []
+            while indx < len(facetlist):
+                savedfacetlist.append(list(facetlist[indx]))
+                indx += 1
+            json.dump(savedfacetlist, outfile)
+        i += 1
+#############_____________________________NODE PERCOLATION________________________________###################
+
 
 
 if __name__ == '__main__':
+    #start = time.time()
+    #path = r'/home/xavier/Documents/Projet/Betti_scm/finalOTU_thresh01/final_thresh01_instance1.json'
+    #higher_order_link_percolation(path, '/home/xavier/Documents/Projet/Betti_scm/percolationtest3/percotest', perc_func=destroy_highest_facet)
+    #exit()
+
     start = time.time()
     path = r'/home/xavier/Documents/Projet/Betti_scm/finalOTU_thresh01/final_thresh01_instance1.json'
-    higher_order_link_percolation(path, '/home/xavier/Documents/Projet/Betti_scm/percolationtest3/percotest', perc_func=destroy_highest_facet)
+    to_skeleton_percolation(path, '/home/xavier/Documents/Projet/Betti_scm/percolationtest3/percotest',
+                                  perc_func=random_facet_to_n_skeleton, n=1)
     exit()
