786427b2de9cfba5bfb18c30612412fa89debe74
Xavier Roy-Pomerleau <xavier.roy-pomerleau.1@ulaval.ca>
2018-11-28 16:19:26 -0500

    Added functions to make it easier to run on a super computer. Most algorithms are based on what Idid in Bar_code.py

--- analyse_betti.py
+++ analyse_betti.py
@@ -56,7 +56,126 @@ def facet_list_to_bipartite(facet_list):
             g.add_edge('v' + str(v), 'f' + str(f))  # differentiate node types
     return g, facet_node_list
 
+
+def compute_betti(facetlist, highest_dim):
+    """
+    This function computes the betti numbers of the j-skeleton of the simplicial complex given in the shape of a facet
+    list.
+    Parameters
+    ----------
+    facetlist (list of list) : Sublists are facets with the index of the nodes in the facet.
+    highest_dim (int) : Highest dimension allowed for the facets. If a facet is of higher dimension in the facet list,
+                        the algorithm breaks it down into it's N choose k faces. The resulting simplicial complexe is
+                        called the j-skeleton, where j = highest_dim.
+
+    Returns
+    -------
+    The (highest_dim - 1) Betti numbers of the simplicial complexe.
+    """
+
+    st = gudhi.SimplexTree()
+    i = 0
+    #print('FACETLIST : ', facetlist)
+    for facet in facetlist:
+
+        # This condition and the loop it contains break a facet in all its n choose k faces. This is more
+        # memory efficient than inserting a big facet because of the way GUDHI's SimplexTree works. The
+        # dimension of the faces has to be at least one unit bigger than the skeleton we use.
+        if len(facet) > highest_dim+1:
+            for face in itertools.combinations(facet, highest_dim+1):
+                st.insert(face)
+        else:
+            st.insert(facet)
+        i += 1
+
+    # This function has to be launched in order to compute the Betti numbers
+    st.persistence()
+
+    return st.betti_numbers()
+
+def compute_and_store_bettis(path, highest_dim, save_path):
+    bettilist = []
+    facetlist = []
+
+    with open(path, 'r') as file:
+        for l in file:
+            facetlist.append([int(x) for x in l.strip().split()])
+
+        bettis = compute_betti(facetlist, highest_dim)
+        if len(bettis) < highest_dim:
+            bettis.extend(0 for i in range(highest_dim - len(bettis)))
+        bettilist.append(bettis)
+        print('icit ', bettis)
+        np.save(save_path + '_bettilist', np.array(bettilist))
+
+
+def compute_and_store_bettis_from_instances(instance_path, idx_range, highest_dim, save_path):
+
+    bettilist = []
+
+    for idx in idx_range:
+        with open(instance_path + str(idx) + '.json', 'r') as file :
+            print('Working on ' + instance_path + str(idx) + '.json')
+
+            bettis = compute_betti(json.load(file), highest_dim)
+            if len(bettis) < highest_dim:
+                bettis.extend(0 for i in range(highest_dim - len(bettis)))
+            bettilist.append(bettis)
+            np.save(save_path + '_bettilist', np.array(bettilist))
+
+def plot_betti_dist(bettiarray_instance, bettiarray_data):
+    """
+    This function plots the distributions of the betti numbers of the instances.
+    Parameters
+    ----------
+    bettiarray_instance (np.array) : saved array using compute_and_store_bettis_from_instances
+    bettiarray_data (np.array) : saved array using compute_and_store_bettis
+
+    Returns
+    -------
+
+    """
+    for column_index in range(0, bettiarray_instance.shape[1]):
+        plt.figure(column_index)
+        n, b, p = plt.hist(bettiarray_instance[:, column_index], bins=np.arange(0, max(bettiarray_instance[:, column_index]) + 0.5), density=True)
+        plt.plot([bettiarray_data[0, column_index], bettiarray_data[0, column_index]], [0, max(n)], color="#ff5b1e")
+        plt.text(-0.29, 25, 'Real system', color="#ff5b1e")
+        # plt.ylim(0, 30)
+        plt.xlabel('Number of Betti ' + str(column_index))
+        plt.ylabel('Normalized count')
+
+    plt.show()
+
 if __name__ == '__main__':
+    instance_path = '/home/xavier/Documents/Projet/Betti_scm/crime/alice1_crime_instance'
+
+    data_path = '/home/xavier/Documents/Projet/Betti_scm/datasets/facet_list_c1_as_simplices.txt'
+
+    idx_range = range(1,101)
+
+    highest_dim = 2
+
+    savepath_betti_instance = instance_path
+
+    savepath_betti_data = '/home/xavier/Documents/Projet/Betti_scm/datasets/facet_list_c1_as_simplicestest'
+
+    compute_and_store_bettis_from_instances(instance_path, idx_range, highest_dim, savepath_betti_instance)
+
+    compute_and_store_bettis(data_path, highest_dim, savepath_betti_data)
+
+
+    bettiarray_instance = np.load(savepath_betti_instance + '_bettilist.npy')
+    bettiarray_data = np.load(savepath_betti_data + '_bettilist.npy')
+
+    plot_betti_dist(bettiarray_instance, bettiarray_data)
+
+
+
+
+
+
+    """
+    #OLD CODE, MIGHT STIL BE USEFULL
     start = time.time()
 
     # Initialise the list that will contain the Betti numbers
@@ -68,6 +187,7 @@ if __name__ == '__main__':
     for idx in range(1, 101):
         with open('/home/xavier/Documents/Projet/Betti_scm/crime/alice1_crime_instance' + str(idx) + '.json', 'r') as file:
             facetlist = json.load(file)
+
             st = gudhi.SimplexTree()
             i = 0
             for facet in facetlist:
@@ -75,8 +195,8 @@ if __name__ == '__main__':
                 # This condition and the loop it contains break a facet in all its n choose k faces. This is more
                 # memory efficient than inserting a big facet because of the way GUDHI's SimplexTree works. The
                 # dimension of the faces has to be at least one unit bigger than the skeleton we use (see next instruction)
-                if len(facet) > 3:
-                    for face in itertools.combinations(facet, 3):
+                if len(facet) > 4:
+                    for face in itertools.combinations(facet, 4):
                         st.insert(face)
                 else:
                     st.insert(facet)
@@ -88,7 +208,7 @@ if __name__ == '__main__':
             # only compute Betti numbers lower than d. The dimension of the skeleton has to be coherent with the
             # dimension of the largest facet allowed, meaning that if we break a facet into its N choose K faces, where
             # K == 3, than the dimension of the skeleton has to be d <= K-1
-            skel = st.get_skeleton(2)
+            skel = st.get_skeleton(3)
             sk = gudhi.SimplexTree()
             for tupl in skel:
                 sk.insert(tupl[0])
@@ -103,7 +223,7 @@ if __name__ == '__main__':
             betti1list.append(Bettis[1])
             #betti2list.append(Bettis[2])
 
-            print('Betti : ', Bettis)
+            print('Betti : ', Bettis, compute_betti(facetlist, 3))
     print(np.mean(betti0list), np.mean(betti1list))
     print('Total time : ', time.time() - start)
 
@@ -202,3 +322,4 @@ if __name__ == '__main__':
     nx.draw_networkx_edges(G, pos)
 
     plt.show()
+    """
