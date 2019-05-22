0a81a4be66361e35313dc3a8aca323b8aff5f216
Xavier Roy-Pomerleau <xavier.roy-pomerleau.1@ulaval.ca>
2019-05-22 15:05:40 -0400

    added a bit of documentation

--- loglin-model.py
+++ loglin-model.py
@@ -1,19 +1,27 @@
 import numpy as np
 import scipy as sp
 from scipy.stats import chi2
+import time
 
+def mle_2x2x2_ind(cont_cube):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis of independence.
+    The formula for the MLEs are obtained via the method proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3. The formulas are summarized in Birch (1963) Maximum Likelihood in Three-Wat Contingency Tables
+    Parameters
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
 
-def chisq_test_2x2x2_ind(cont_cube):
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of independence
+    -------
+
+    """
     #Computes the chisquare statistics and its p-value for a 2X2X2 contingency table under the total independence hypothesis
 
     m__k = np.sum(np.sum(cont_cube, axis=2), axis=1)
     m_j_ = np.sum(np.sum(cont_cube, axis=1), axis=0)
     mi__ = np.sum(np.sum(cont_cube, axis=2), axis=0)
-
-
     n = np.sum(cont_cube)
-
-    df = 1 # TODO df = 1???
 
     row_props = mi__/n
     col_props = m_j_/n
@@ -25,20 +33,27 @@ def chisq_test_2x2x2_ind(cont_cube):
 
                 expected[k,i,j] = row_props[i]*col_props[j]*depth_props[i,j]*n
 
-    test_stat = np.sum((cont_cube - expected) ** 2 / expected)
-    p_val = chi2.sf(test_stat, df)
+    return expected
 
-    return test_stat, p_val
+def mle_2x2x2_AB_C(cont_cube):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis Variable 3 is independent of variable
+    1 and 2 together.
+    The formula for the MLEs are obtained via the method proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3. The formulas are summarized in Birch (1963) Maximum Likelihood in Three-Wat Contingency Tables
+    Parameters
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
 
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of Variable 3 is independent of variable
+    1 and 2 together.
+    -------
 
-def chisq_test_2x2x2_AB_C(cont_cube):
-    #Computes the chisquare statistics and its p-value for a 2X2X2 contingency table under the total independence hypothesis
+    """
 
     m__k = np.sum(np.sum(cont_cube, axis=2), axis=1)
     mij_ = np.sum(cont_cube, axis=0)
     n = np.sum(cont_cube)
-
-    df = 1 # TODO df = 1???
 
     expected = np.random.rand(2,2,2)
     for i in range(2):
@@ -47,13 +62,23 @@ def chisq_test_2x2x2_AB_C(cont_cube):
 
                 expected[k,i,j] = mij_[i,j]*m__k[k]/n
 
-    test_stat = np.sum((cont_cube - expected) ** 2 / expected)
-    p_val = chi2.sf(test_stat, df)
+    return expected
 
-    return test_stat, p_val
+def mle_2x2x2_AC_B(cont_cube):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis Variable 2 is independent of variable
+    1 and 3 together.
+    The formula for the MLEs are obtained via the method proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3. The formulas are summarized in Birch (1963) Maximum Likelihood in Three-Wat Contingency Tables
+    Parameters
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
 
-def chisq_test_2x2x2_AC_B(cont_cube):
-    #Computes the chisquare statistics and its p-value for a 2X2X2 contingency table under the total independence hypothesis
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of Variable 2 is independent of variable
+    1 and 3 together.
+    -------
+
+    """
     m_j_ = np.sum(np.sum(cont_cube, axis=1), axis=0)
     mi_k = np.sum(cont_cube, axis=2).T
     n = np.sum(cont_cube)
@@ -72,8 +97,21 @@ def chisq_test_2x2x2_AC_B(cont_cube):
 
     return test_stat, p_val
 
-def chisq_test_2x2x2_BC_A(cont_cube):
-    #Computes the chisquare statistics and its p-value for a 2X2X2 contingency table under the total independence hypothesis
+def mle_2x2x2_BC_A(cont_cube):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis Variable 1 is independent of variable
+    2 and 3 together.
+    The formula for the MLEs are obtained via the method proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3. The formulas are summarized in Birch (1963) Maximum Likelihood in Three-Wat Contingency Tables
+    Parameters
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
+
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of Variable 1 is independent of variable
+    2 and 3 together.
+    -------
+
+    """
     mi__ = np.sum(np.sum(cont_cube, axis=0), axis=1)
     m_jk = np.sum(cont_cube, axis=1).T
     n = np.sum(cont_cube)
@@ -87,21 +125,28 @@ def chisq_test_2x2x2_BC_A(cont_cube):
 
                 expected[k,i,j] = m_jk[j,k]*mi__[i]/n
 
-    test_stat = np.sum((cont_cube - expected) ** 2 / expected)
-    p_val = chi2.sf(test_stat, df)
+    return expected
 
-    return test_stat, p_val
+def mle_2x2x2_AC_BC(cont_cube):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis Variable 1 and 2 are indepndent in
+     each layer of variable 3.
+    1 and 2 together.
+    The formula for the MLEs are obtained via the method proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3. The formulas are summarized in Birch (1963) Maximum Likelihood in Three-Wat Contingency Tables
+    Parameters
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
 
-def chisq_test_2x2x2_AC_BC(cont_cube):
-    #Computes the chisquare statistics and its p-value for a 2X2X2 contingency table under the total independence hypothesis
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of Variable 1 and 2 are indepndent in
+     each layer of variable 3.
+    -------
+
+    """
 
     m_jk = np.sum(cont_cube, axis=1).T
     mi_k = np.sum(cont_cube, axis=2).T
     m__k = np.sum(np.sum(cont_cube, axis=2), axis=1)
-
-    n = np.sum(cont_cube)
-
-    df = 1 # TODO df = 1???
 
     expected = np.random.rand(2,2,2)
     for i in range(2):
@@ -110,21 +155,28 @@ def chisq_test_2x2x2_AC_BC(cont_cube):
 
                 expected[k,i,j] = m_jk[j,k]*mi_k[i,k]/m__k[k]
 
-    test_stat = np.sum((cont_cube - expected) ** 2 / expected)
-    p_val = chi2.sf(test_stat, df)
+    return expected
 
-    return test_stat, p_val
+def mle_2x2x2_AB_BC(cont_cube):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis Variable 1 and 3 are indepndent in
+     each layer of variable 2.
+    1 and 2 together.
+    The formula for the MLEs are obtained via the method proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3. The formulas are summarized in Birch (1963) Maximum Likelihood in Three-Wat Contingency Tables
+    Parameters
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
 
-def chisq_test_2x2x2_AB_BC(cont_cube):
-    #Computes the chisquare statistics and its p-value for a 2X2X2 contingency table under the total independence hypothesis
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of Variable 1 and 3 are indepndent in
+     each layer of variable 2.
+    -------
+
+    """
 
     mij_ = np.sum(cont_cube, axis=0)
     m_jk = np.sum(cont_cube, axis=1).T
     m_j_ = np.sum(np.sum(cont_cube, axis=1), axis=0)
-
-    n = np.sum(cont_cube)
-
-    df = 1 # TODO df = 1???
 
     expected = np.random.rand(2,2,2)
     for i in range(2):
@@ -133,22 +185,29 @@ def chisq_test_2x2x2_AB_BC(cont_cube):
 
                 expected[k,i,j] = m_jk[j,k]*mij_[i,j]/m_j_[j]
 
-    test_stat = np.sum((cont_cube - expected) ** 2 / expected)
-    p_val = chi2.sf(test_stat, df)
-
-    return test_stat, p_val
+    return expected
 
 
-def chisq_test_2x2x2_AB_AC(cont_cube):
-    # Computes the chisquare statistics and its p-value for a 2X2X2 contingency table under the total independence hypothesis
+def mle_2x2x2_AB_AC(cont_cube):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis Variable 2 and 3 are indepndent in
+     each layer of variable 1.
+    1 and 2 together.
+    The formula for the MLEs are obtained via the method proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3. The formulas are summarized in Birch (1963) Maximum Likelihood in Three-Wat Contingency Tables
+    Parameters
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
+
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of Variable 2 and 3 are indepndent in
+     each layer of variable 1.
+    -------
+
+    """
 
     mij_ = np.sum(cont_cube, axis=0)
     mi_k = np.sum(cont_cube, axis=2).T
     mi__ = np.sum(np.sum(cont_cube, axis=0), axis=1)
-
-    n = np.sum(cont_cube)
-
-    df = 1  # TODO df = 1???
 
     expected = np.random.rand(2, 2, 2)
     for i in range(2):
@@ -156,12 +215,22 @@ def chisq_test_2x2x2_AB_AC(cont_cube):
             for k in range(2):
                 expected[k, i, j] = mi_k[i,k] * mij_[i, j] / mi__[i]
 
-    test_stat = np.sum((cont_cube - expected) ** 2 / expected)
-    p_val = chi2.sf(test_stat, df)
+    return expected
 
-    return test_stat, p_val
+def iterative_proportional_fitting_AB_AC_BC(cont_cube, delta=0.01):
+    """
+    Computes the maximum likelihood estimates of a 2X2X2 table under the hypothesis no three-factor interaction.
+    1 and 2 together.
+    The formula for the MLEs are obtained via the iterative proportional fitting proposed in Bishop et Al. Discrete multivariate analysis
+    chapter 3.
+    ----------
+    cont_cube (2X2X2 numpy array) : Contingency cube of the three variables
+    delta (float) : If the difference between the last estimates and the current estimates, we accept the estimates.
 
-def iterative_proportional_fitting_ABC(cont_cube, delta=0.01):
+    Returns (2X2X2 numpy array) : Contingency cube with the expected values under the hypothesis of no three-factor interaction
+    -------
+
+    """
 
     xij_ = np.sum(cont_cube, axis=0)
     xi_k = np.sum(cont_cube, axis=2).T
@@ -199,6 +268,7 @@ def iterative_proportional_fitting_ABC(cont_cube, delta=0.01):
     return mijk
 
 def iterative_proportional_fitting_AC_BC(cont_cube, delta=0.01):
+    "See the equivalent MLE function above"
 
     #xij_ = np.sum(cont_cube, axis=0)
     xi_k = np.sum(cont_cube, axis=2).T
@@ -236,7 +306,7 @@ def iterative_proportional_fitting_AC_BC(cont_cube, delta=0.01):
     return mijk
 
 def iterative_proportional_fitting_AB_BC(cont_cube, delta=0.01):
-
+    "See the equivalent MLE function above"
     xij_ = np.sum(cont_cube, axis=0)
     #xi_k = np.sum(cont_cube, axis=2).T
     x_jk = np.sum(cont_cube, axis=1).T
@@ -273,7 +343,7 @@ def iterative_proportional_fitting_AB_BC(cont_cube, delta=0.01):
     return mijk
 
 def iterative_proportional_fitting_AB_AC(cont_cube, delta=0.01):
-
+    "See the equivalent MLE function above"
     xij_ = np.sum(cont_cube, axis=0)
     xi_k = np.sum(cont_cube, axis=2).T
     #x_jk = np.sum(cont_cube, axis=1).T
@@ -310,7 +380,7 @@ def iterative_proportional_fitting_AB_AC(cont_cube, delta=0.01):
     return mijk
 
 def iterative_proportional_fitting_BC_A(cont_cube, delta=0.01):
-
+    "See the equivalent MLE function above"
     x_jk = np.sum(cont_cube, axis=1).T
     xi__ = np.sum(np.sum(cont_cube, axis=0), axis=1)
 
@@ -340,7 +410,7 @@ def iterative_proportional_fitting_BC_A(cont_cube, delta=0.01):
     return mijk
 
 def iterative_proportional_fitting_AB_C(cont_cube, delta=0.01):
-
+    "See the equivalent MLE function above"
     xij_ = np.sum(cont_cube, axis=0)
     x__k = np.sum(np.sum(cont_cube, axis=2), axis=1)
 
@@ -370,7 +440,7 @@ def iterative_proportional_fitting_AB_C(cont_cube, delta=0.01):
     return mijk
 
 def iterative_proportional_fitting_AC_B(cont_cube, delta=0.01):
-
+    "See the equivalent MLE function above"
     xi_k = np.sum(cont_cube, axis=2).T
     x_j_ = np.sum(np.sum(cont_cube, axis=1), axis=0)
 
@@ -400,7 +470,7 @@ def iterative_proportional_fitting_AC_B(cont_cube, delta=0.01):
     return mijk
 
 def iterative_proportional_fitting_ind(cont_cube, delta=0.01):
-
+    "See the equivalent MLE function above"
     xi__ = np.sum(np.sum(cont_cube, axis=0), axis=1)
     x_j_ = np.sum(np.sum(cont_cube, axis=1), axis=0)
     x__k = np.sum(np.sum(cont_cube, axis=2), axis=1)
@@ -450,6 +520,7 @@ if __name__ == '__main__':
     xijk[1, 1, 0] = 133
     xijk[1, 0, 1] = 31
     xijk[1, 1, 1] = 209
+
     print(iterative_proportional_fitting_ind(xijk, delta=0.000001))
     #chisq_test_2x2x2_AC_B(xijk)
 
